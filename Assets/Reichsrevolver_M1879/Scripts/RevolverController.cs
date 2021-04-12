c42ee8908ac5d4c7d9a923fe56c44df4eebf0a0c
Avadh Patel <avadh23p@gmail.com>
2021-04-12 16:23:27 -0700

    gun and stuff kind of

---
+++ Assets/Reichsrevolver_M1879/Scripts/RevolverController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RevolverController : MonoBehaviour {
+	public float revRotSpeed;			// revolver rotation speed
+	public Transform cylTr;				// ref to cylinder of revolver
+
+	private bool rotatingRev = false;
+	private bool rotatingCyl = false;
+	private float endAngle = 60F;		// rotated angle
+	private AudioSource audioSource;
+	private Animator animator;
+
+	void Start () {
+		cylTr = cylTr.transform;
+		audioSource = GetComponent<AudioSource>();
+		animator = GetComponent<Animator>();
+	}
+
+	void Update () {
+		if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                Fire();
+            }
+        }
+
+		if (rotatingRev)
+			transform.Rotate (Vector3.up * revRotSpeed * Time.deltaTime);
+
+		if (rotatingCyl) {
+			RotateCyl ();
+		}
+	}
+
+	public void FlipRevState () {
+		rotatingRev = !rotatingRev;
+	}
+
+	public void RotateCyl () {
+		if (endAngle == 360F && cylTr.localRotation.eulerAngles.y < 60F) {
+			endAngle = 0F;
+		}
+		if (cylTr.localRotation.eulerAngles.y < endAngle ) {
+			rotatingCyl = true;
+			Quaternion target = Quaternion.Euler (0, endAngle, 0); 
+			cylTr.localRotation = Quaternion.RotateTowards (cylTr.localRotation, target, Time.deltaTime * 100F);
+		} else {
+			rotatingCyl = false;
+			endAngle += 60F;
+		}
+	}
+
+	public void Fire() {
+    	audioSource.PlayOneShot(audioSource.clip);
+		animator.SetTrigger("Fire");
+		RotateCyl();
+	}
+}
