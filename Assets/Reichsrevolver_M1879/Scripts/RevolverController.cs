e5911e4363b37e061a38643f73923ad436556289
Avadh Patel <avadh23p@gmail.com>
2021-04-19 15:48:40 -0700

    gun working, sound not, built not

--- Assets/Reichsrevolver_M1879/Scripts/RevolverController.cs
+++ Assets/Reichsrevolver_M1879/Scripts/RevolverController.cs
@@ -10,24 +10,29 @@ public class RevolverController : MonoBehaviour {
 	private float endAngle = 60F;		// rotated angle
 	private AudioSource audioSource;
 	private Animator animator;
-	private ParticleSystem particleSystem;
+	private ParticleSystem particles;
 
 	void Start () {
 		cylTr = cylTr.transform;
 		audioSource = GetComponent<AudioSource>();
 		animator = GetComponent<Animator>();
-		particleSystem = transform.Find("MuzzleFlashEffect").GetComponent<ParticleSystem>();
+		particles = transform.Find("MuzzleFlashEffect").GetComponent<ParticleSystem>();
+		Fire();
 	}
 
 	void Update () {
 		if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-            if (Input.GetButton("Fire") || touch.phase == TouchPhase.Began)
+            if (touch.phase == TouchPhase.Began)
             {
                 Fire();
             }
         }
+
+		if (Input.GetButton("Fire")) {
+			Fire();
+		}
 
 		if (rotatingRev)
 			transform.Rotate (Vector3.up * revRotSpeed * Time.deltaTime);
@@ -58,7 +63,7 @@ public class RevolverController : MonoBehaviour {
 	public void Fire() {
     	audioSource.PlayOneShot(audioSource.clip);
 		animator.SetTrigger("Fire");
-		particleSystem.Play();
+		particles.Play();
 		RotateCyl();
 	}
 }
